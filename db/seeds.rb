333a971eca5d0515dda5a087ee85d623b82932bb
David Myers <davidrmyersii@gmail.com>
2019-01-29 09:36:55 -0500

    Add more workouts

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,18 @@ Activity.where(name: 'Deadlift').first_or_create!
 Activity.where(name: 'Power Clean').first_or_create!
 Activity.where(name: 'Bench Press').first_or_create!
 Activity.where(name: 'Snatch').first_or_create!
+Activity.where(name: 'Overhead Squat').first_or_create!
+Activity.where(name: 'Squat Clean').first_or_create!
+Activity.where(name: 'Sumo Deadlift').first_or_create!
+Activity.where(name: 'Push Up').first_or_create!
+Activity.where(name: 'Pull Up').first_or_create!
+Activity.where(name: 'Box Jump').first_or_create!
+Activity.where(name: 'Double Under').first_or_create!
+Activity.where(name: 'Wall Ball').first_or_create!
+Activity.where(name: 'Muscle Up').first_or_create!
+Activity.where(name: 'Pull Up (C2B)').first_or_create!
+Activity.where(name: 'Pull Up (Strict)').first_or_create!
+Activity.where(name: 'Thruster').first_or_create!
+Activity.where(name: 'Cluster').first_or_create!
+Activity.where(name: 'Hang Snatch').first_or_create!
+Activity.where(name: 'Rope Climb').first_or_create!
